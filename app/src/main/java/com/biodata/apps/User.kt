78e074f6c954b90e60dd301e1f1d5e85b8e74896
Agung Subastian <agungsubastian1963@gmail.com>
2020-10-21 15:15:24 +0700

    set editable

--- app/src/main/java/com/biodata/apps/User.kt
+++ app/src/main/java/com/biodata/apps/User.kt
@@ -1,9 +1,13 @@
 package com.biodata.apps
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class User(
     val nama: String? = "",
     val gender: String? = "",
     val tempat: String? = "",
     val tanggal: String? = "",
     val alamat: String? = ""
-)
+) : Parcelable
